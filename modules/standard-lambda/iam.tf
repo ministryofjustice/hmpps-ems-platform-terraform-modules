0016a40ddbea25a67bdf78cadc3398b6d16103da
Craig Gumbley <craiggumbley@gmail.com>
2020-09-24 18:03:53 +0100

    Adds a standard lambda module

---
+++ modules/standard-lambda/iam.tf
@@ -0,0 +1,45 @@
+data "aws_iam_policy_document" "assume" {
+  statement {
+    sid = "AllowLambdaAssumeRole"
+    effect = "Allow"
+    actions = [
+      "sts:AssumeRole"
+    ]
+    principals {
+      type = "Service"
+      identifiers = [
+        "lambda.amazonaws.com"
+      ]
+    }
+  }
+}
+
+
+data "aws_iam_policy_document" "lambda_policy" {
+  source_json = var.iam_policy
+
+  statement {
+    sid = "AllowCloudwatchLogs"
+    effect = "Allow"
+    actions = [
+          "logs:CreateLogGroup",
+          "logs:CreateLogStream",
+          "logs:PutLogEvents"
+    ]
+    resources = [
+      "arn:aws:logs:*:*:*"
+    ]
+  }
+}
+
+resource "aws_iam_role" "this" {
+  name = local.role_name
+  assume_role_policy = data.aws_iam_policy_document.assume.json
+}
+
+
+resource "aws_iam_role_policy" "this" {
+  name = local.role_policy_name
+  role = aws_iam_role.this.id
+  policy = data.aws_iam_policy_document.lambda_policy.json
+}
