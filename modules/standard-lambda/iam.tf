fc6ec0857dcc909cf7394d32d3d646fefe805e4e
Craig Gumbley <craiggumbley@gmail.com>
2020-09-24 18:04:55 +0100

    Formatting

--- modules/standard-lambda/iam.tf
+++ modules/standard-lambda/iam.tf
@@ -1,6 +1,6 @@
 data "aws_iam_policy_document" "assume" {
   statement {
-    sid = "AllowLambdaAssumeRole"
+    sid    = "AllowLambdaAssumeRole"
     effect = "Allow"
     actions = [
       "sts:AssumeRole"
@@ -19,12 +19,12 @@ data "aws_iam_policy_document" "lambda_policy" {
   source_json = var.iam_policy
 
   statement {
-    sid = "AllowCloudwatchLogs"
+    sid    = "AllowCloudwatchLogs"
     effect = "Allow"
     actions = [
-          "logs:CreateLogGroup",
-          "logs:CreateLogStream",
-          "logs:PutLogEvents"
+      "logs:CreateLogGroup",
+      "logs:CreateLogStream",
+      "logs:PutLogEvents"
     ]
     resources = [
       "arn:aws:logs:*:*:*"
@@ -33,13 +33,13 @@ data "aws_iam_policy_document" "lambda_policy" {
 }
 
 resource "aws_iam_role" "this" {
-  name = local.role_name
+  name               = local.role_name
   assume_role_policy = data.aws_iam_policy_document.assume.json
 }
 
 
 resource "aws_iam_role_policy" "this" {
-  name = local.role_policy_name
-  role = aws_iam_role.this.id
+  name   = local.role_policy_name
+  role   = aws_iam_role.this.id
   policy = data.aws_iam_policy_document.lambda_policy.json
 }
