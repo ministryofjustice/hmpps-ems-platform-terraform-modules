146df24279c0e86473c4627383bc6d0dc9492795
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-09-28 15:39:45 +0100

    Removing comments

--- modules/blackhole-monitor/lambda.tf
+++ modules/blackhole-monitor/lambda.tf
@@ -4,28 +4,12 @@ data "archive_file" "source" {
   output_path = "${path.module}/files/blackhole-monitor.zip"
 }
 
-# data "archive_file" "layer" {
-#   type        = "zip"
-#   source_dir  = "${path.module}/layers"
-#   output_path = "${path.module}/files/blackhole-monitor-layer.zip"
-# }
-
-# resource "aws_lambda_layer_version" "this" {
-#   filename   = data.archive_file.layer.output_path
-#   # layer_name = local.lambda_function_layer_name
-
-#   source_code_hash    = filebase64sha256(data.archive_file.layer.output_path)
-#   compatible_runtimes = ["${local.lambda_function_runtime}"]
-# }
 
 resource "aws_lambda_function" "this" {
   filename      = data.archive_file.source.output_path
   function_name = local.lambda_function_name
   role          = aws_iam_role.lambda.arn
   handler       = "monitor.lambda_handler"
-  # layers = [
-  #   aws_lambda_layer_version.this.arn
-  # ]
 
   source_code_hash = filebase64sha256(data.archive_file.source.output_path)
 
