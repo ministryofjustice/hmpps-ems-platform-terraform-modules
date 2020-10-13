a10576353400338671f1b751675b0364831c09fd
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-13 12:31:29 +0100

    Added GuardDuty SL var to account baseline

--- modules/account-baseline/guardduty.tf
+++ modules/account-baseline/guardduty.tf
@@ -1,5 +1,6 @@
 module "baseline_guardduty" {
   source                             = "./modules/baseline-guardduty"
   resource_name_prefix               = var.resource_name_prefix
+  sl_firehose_destination_guardduty  = var.sl_firehose_destination_guardduty
   tags                               = var.tags
 }
