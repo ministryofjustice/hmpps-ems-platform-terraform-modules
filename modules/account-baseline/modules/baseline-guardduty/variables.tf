da1c7b883d73e7b006f30a8787535b5728d82a95
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-13 14:01:25 +0100

    Update cloudwatch_log_group_name local

--- modules/account-baseline/modules/baseline-guardduty/variables.tf
+++ modules/account-baseline/modules/baseline-guardduty/variables.tf
@@ -29,7 +29,7 @@ variable "publishing_frequency" {
 
 
 locals {
-  cloudwatch_log_group_name  = "${var.resource_name_prefix}-guardduty-log-group"
+  cloudwatch_log_group_name  = "/aws/events/${var.resource_name_prefix}-guardduty-log-group"
   cloudwatch_event_rule_name = "${var.resource_name_prefix}-guardduty-event-rule"
   cloudwatch_event_target_id = "${var.resource_name_prefix}-guardduty-event-target"
   cloudwatch_iam_policy_name = "${var.resource_name_prefix}-guardduty-policy"
