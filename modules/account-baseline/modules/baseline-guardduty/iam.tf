49df7f7bfd88d08e49141b452a33c752cf5f1516
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-13 13:46:06 +0100

    Update resources for cloudwatch policy

--- modules/account-baseline/modules/baseline-guardduty/iam.tf
+++ modules/account-baseline/modules/baseline-guardduty/iam.tf
@@ -3,7 +3,7 @@ data "aws_iam_policy_document" "cloudwatch" {
     sid       = "AWSCloudTrail"	
     effect    = "Allow"	
     actions   = ["logs:*"]	
-    resources = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.this.name}*"]	
+    resources = ["*"]	
   }	
 }
 
