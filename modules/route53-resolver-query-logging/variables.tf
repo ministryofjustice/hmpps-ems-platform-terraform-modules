3b4251ebf5eb02dd752e3d01b69511194b647da4
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-13 15:30:42 +0100

    Added variables

---
+++ modules/route53-resolver-query-logging/variables.tf
@@ -0,0 +1,16 @@
+variable "resource_name_prefix" {
+  type        = string
+  description = "This is the prefix that will be applied to all resources deployed by this module."
+}
+
+
+variable "tags" {
+  type        = map(string)
+  description = "A list of tags that will be applied to deployed resources."
+}
+
+
+locals {
+  cloudwatch_log_group_name     = "${var.resource_name_prefix}-route53-resolver-query-logging-log-group"
+  route53_query_log_config_name = "${var.resource_name_prefix}-route53-resolver-query-log"
+}
