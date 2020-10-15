ed74596c6831ba43f088741f0c5bafd2c6f4861c
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-15 17:51:07 +0100

    Added more statements to KMS policy

--- modules/account-baseline/modules/baseline-cloudtrail/kms.tf
+++ modules/account-baseline/modules/baseline-cloudtrail/kms.tf
@@ -10,6 +10,54 @@ data "aws_iam_policy_document" "kms" {
     resources = ["*"]
   }
   statement {
+    sid     = "AllowCloudTrailEncryption"
+    effect  = "Allow"
+    actions = ["kms:GenerateDataKey*"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    resources = ["*"]
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
+    }
+  }
+  statement {
+    sid     = "AllowCloudTrailDescribeKey"
+    effect  = "Allow"
+    actions = ["kms:DescribeKey"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid     = "AllowPrincipalDecryption"
+    effect  = "Allow"
+    actions = ["kms:Decrypt", "kms:ReEncryptFrom"]
+    principals {
+      type        = "Service"
+      identifiers = ["cloudtrail.amazonaws.com"]
+    }
+    resources = ["*"]
+    condition {
+      test     = "StringEquals"
+      variable = "kms:CallerAccount"
+      values   = "${data.aws_caller_identity.current.account_id}"
+    }
+    condition {
+      test     = "StringLike"
+      variable = "kms:EncryptionContext:aws:cloudtrail:arn"
+      values   = "arn:aws:cloudtrail:*:${data.aws_caller_identity.current.account_id}:trail/*"
+    }
+  }
+
+
+
+  statement {
     sid     = "AllowCloudWatchLogsGroup"
     effect  = "Allow"
     actions = ["kms:Encrypt*", "kms:Decrypt*", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:Describe*"]
