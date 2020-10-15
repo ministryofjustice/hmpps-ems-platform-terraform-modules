be2d79881aabebd2dffd696bd104962087ea1e8f
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-15 16:51:03 +0100

    Added KMS Key

---
+++ modules/account-baseline/modules/baseline-cloudtrail/kms.tf
@@ -0,0 +1,13 @@
+resource "aws_kms_key" "this" {
+  is_enabled               = true
+  key_usage                = "ENCRYPT_DECRYPT"
+  customer_master_key_spec = "SYMMETRIC_DEFAULT"
+  deletion_window_in_days  = 30
+  # policy =
+  tags = var.tags
+}
+
+resource "aws_kms_alias" "this" {
+  name          = local.cloudtrail_kms_key_alias_name
+  target_key_id = aws_kms_key.this.key_id
+}
