e12420070e23357d6483ce2ce44cad439f357e8d
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-15 17:40:47 +0100

    Add condition to KMS policy

--- modules/account-baseline/modules/baseline-cloudtrail/kms.tf
+++ modules/account-baseline/modules/baseline-cloudtrail/kms.tf
@@ -18,6 +18,11 @@ data "aws_iam_policy_document" "kms" {
       identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
     }
     resources = ["*"]
+    condition {
+      test     = "ArnEquals"
+      variable = "kms:EncryptionContext:aws:logs:arn"
+      values   = ["arn:aws:logs:${data.aws_region.current.name}:${data.aws_caller_identity.current.account_id}:log-group:${aws_cloudwatch_log_group.cloudwatch.name}"]
+    }
   }
 }
 
