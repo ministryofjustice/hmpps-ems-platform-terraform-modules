7517486b2882b12f6f0df876117ea7d175ce7a58
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-15 17:27:01 +0100

    Added KMS Policy

--- modules/account-baseline/modules/baseline-cloudtrail/kms.tf
+++ modules/account-baseline/modules/baseline-cloudtrail/kms.tf
@@ -1,11 +1,34 @@
+data "aws_iam_policy_document" "kms" {
+  statement {
+    sid     = "EnableIAMUserPermissions"
+    effect  = "Allow"
+    actions = ["kms:*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["arn:aws:iam::${data.aws_caller_identity.current.account_id}:root"]
+    }
+    resources = ["*"]
+  }
+  statement {
+    sid     = "AllowCloudWatchLogsGroup"
+    effect  = "Allow"
+    actions = ["kms:Encrypt*", "kms:Decrypt*", "kms:ReEncrypt*", "kms:GenerateDataKey*", "kms:Describe*"]
+    principals {
+      type        = "AWS"
+      identifiers = ["logs.${data.aws_region.current.name}.amazonaws.com"]
+    }
+    resources = ["*"]
+  }
+}
+
 resource "aws_kms_key" "this" {
   is_enabled               = true
   key_usage                = "ENCRYPT_DECRYPT"
   customer_master_key_spec = "SYMMETRIC_DEFAULT"
   enable_key_rotation      = true
   deletion_window_in_days  = 30
-  # policy =
-  tags = var.tags
+  policy                   = data.aws_iam_policy_document.kms.json
+  tags                     = var.tags
 }
 
 resource "aws_kms_alias" "this" {
