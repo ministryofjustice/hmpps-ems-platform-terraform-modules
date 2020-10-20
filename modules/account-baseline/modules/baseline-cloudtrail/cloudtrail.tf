077cae22946da05f0a3eca34b4fc34b47dc33cde
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2020-10-20 11:00:03 +0100

    EMB-132 Added CloudTrail data events

--- modules/account-baseline/modules/baseline-cloudtrail/cloudtrail.tf
+++ modules/account-baseline/modules/baseline-cloudtrail/cloudtrail.tf
@@ -6,5 +6,18 @@ resource "aws_cloudtrail" "cloudtrail" {
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudwatch.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudwatch.arn
   kms_key_id                    = aws_kms_key.this.arn
+  event_selector {
+    include_management_events = true
+    read_write_type           = "All"
+
+    data_resource {
+      type   = "AWS::S3::Object"
+      values = ["arn:aws:s3"]
+    }
+    data_resource {
+      type   = "AWS::Lambda::Function"
+      values = ["arn:aws:lambda"]
+    }
+  }
   tags                          = var.tags
 }
