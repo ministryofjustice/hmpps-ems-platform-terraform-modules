e3c37b037a3275a130a72082531c8cba61c77e9e
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2021-04-06 19:46:47 +0100

    EMSP-174 Set CloudTrail to Multi Region (#24)

--- modules/account-baseline/modules/baseline-cloudtrail/cloudtrail.tf
+++ modules/account-baseline/modules/baseline-cloudtrail/cloudtrail.tf
@@ -3,6 +3,7 @@ resource "aws_cloudtrail" "cloudtrail" {
   s3_bucket_name                = "hmpps-sl-awscloudtrail-log-bucket" // TODO - https://github.com/ministryofjustice/hmpps-ems-platform-terraform-modules/issues/22
   include_global_service_events = true
   enable_log_file_validation    = true
+  is_multi_region_trail         = true
   cloud_watch_logs_group_arn    = "${aws_cloudwatch_log_group.cloudwatch.arn}:*"
   cloud_watch_logs_role_arn     = aws_iam_role.cloudwatch.arn
   kms_key_id                    = aws_kms_key.this.arn
