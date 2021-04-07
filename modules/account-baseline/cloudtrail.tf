224238c64dc9f3019040a6d0acbb200bd67287a0
Jacob Woffenden <jacob.woffenden@baesystems.com>
2021-04-07 16:12:22 +0100

    Removing CloudTrail/CloudWatch Shared Logging Removing legacy CloudTrail S3

--- modules/account-baseline/cloudtrail.tf
+++ modules/account-baseline/cloudtrail.tf
@@ -1,6 +1,5 @@
 module "baseline_cloudtrail" {
   source                             = "./modules/baseline-cloudtrail"
   resource_name_prefix               = var.resource_name_prefix
-  sl_firehose_destination_cloudtrail = var.sl_firehose_destination_cloudtrail
   tags                               = var.tags
 }
