1768194b9eb9b690103776c31cdc4ce54be0a742
Jacob Woffenden <jacob.woffenden@digital.justice.gov.uk>
2021-06-03 18:39:44 +0100

    Added Further ReadOnlyAccess Roles to the ReadOnlyRole (#26)

--- modules/account-baseline/main.tf
+++ modules/account-baseline/main.tf
@@ -12,11 +12,21 @@ data "aws_iam_policy" "ReadOnlyAccess" {
   arn = "arn:aws:iam::aws:policy/ReadOnlyAccess"
 }
 
-
 data "aws_iam_policy" "AdministratorAccess" {
   arn = "arn:aws:iam::aws:policy/AdministratorAccess"
 }
 
+data "aws_iam_policy" "AWSConfigUserAccess" {
+  arn = "arn:aws:iam::aws:policy/AWSConfigUserAccess"
+}
+
+data "aws_iam_policy" "AmazonGuardDutyReadOnlyAccess" {
+  arn = "arn:aws:iam::aws:policy/AmazonGuardDutyReadOnlyAccess"
+}
+
+data "aws_iam_policy" "AWSSecurityHubReadOnlyAccess" {
+  arn = "arn:aws:iam::aws:policy/AWSSecurityHubReadOnlyAccess"
+}
 
 locals {
   user_malicious_activity_deny_policy_arn = "arn:aws:iam::${data.aws_caller_identity.current.account_id}:policy/hmpps-user-malicious-activity-deny-policy"
