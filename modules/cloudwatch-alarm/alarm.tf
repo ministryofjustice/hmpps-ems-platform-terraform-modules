23455b048cb6aa1e6efe0820a4b79f84f02fb67b
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2023-10-05 09:35:38 +0100

    Create a new module for creating aws cloudwatch metric alarms (#52)

---
+++ modules/cloudwatch-alarm/alarm.tf
@@ -0,0 +1,26 @@
+resource "aws_cloudwatch_metric_alarm" "this" {
+  alarm_name        = var.alarm_name
+  alarm_description = "Run-book: ${var.runbook_url}"
+  actions_enabled   = true
+
+  alarm_actions             = var.alarm_actions
+  ok_actions                = var.ok_actions
+  insufficient_data_actions = var.insufficient_data_actions
+
+  comparison_operator = var.comparison_operator
+  evaluation_periods  = var.evaluation_periods
+  threshold           = var.threshold
+  unit                = var.unit
+
+  datapoints_to_alarm = var.datapoints_to_alarm
+  treat_missing_data  = var.treat_missing_data
+
+  metric_name = var.metric_name
+  namespace   = var.namespace
+  period      = var.period
+  statistic   = var.statistic
+
+  dimensions = var.dimensions
+
+  tags = var.tags
+}
