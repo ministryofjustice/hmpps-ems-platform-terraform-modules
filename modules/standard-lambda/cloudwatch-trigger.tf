8210b26b7a1c8b6a66aa182b8eb54a10bc333675
Ben Carter <ben.carter@justice.gov.uk>
2023-10-31 14:00:46 +0000

    ELM-627: Add the ability to disable the cloudwatch event creation in the lambda module

--- modules/standard-lambda/cloudwatch-trigger.tf
+++ modules/standard-lambda/cloudwatch-trigger.tf
@@ -1,5 +1,7 @@
 # --- Trigger the lambda every 5 minutes
 resource "aws_cloudwatch_event_rule" "this" {
+  count = var.enable_schedule ? 1 : 0
+
   name                = local.cloudwatch_event_rule_name
   description         = "A rule that triggers the lambda on a configurable schedule"
   schedule_expression = var.trigger_schedule
@@ -8,15 +10,19 @@ resource "aws_cloudwatch_event_rule" "this" {
 
 
 resource "aws_cloudwatch_event_target" "this" {
+  count = var.enable_schedule ? 1 : 0
+
   target_id = local.cloudwatch_event_target_id
-  rule      = aws_cloudwatch_event_rule.this.name
+  rule      = aws_cloudwatch_event_rule.this[0].name
   arn       = aws_lambda_function.this.arn
 }
 
 
 resource "aws_lambda_permission" "this" {
+  count = var.enable_schedule ? 1 : 0
+
   action        = "lambda:InvokeFunction"
   function_name = aws_lambda_function.this.function_name
   principal     = "events.amazonaws.com"
-  source_arn    = aws_cloudwatch_event_rule.this.arn
+  source_arn    = aws_cloudwatch_event_rule.this[0].arn
 }
