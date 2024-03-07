a5cf1117f721c9ee991d23fbba396a7111d972f0
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2024-03-07 13:35:54 +0000

    ELM-1137: Create a role that can be used by the soc and deployed to all accounts (#64)
    Co-authored-by: Adrian Wilkins <adrian.wilkins@madetech.com>

---
+++ modules/security-operations-centre-role/README.md
@@ -0,0 +1,15 @@
+# Security Operations Centre Role
+
+A reusable, configurable role that can be easily deployed into an AWS account that grants access to security events.
+
+Currently supported:
+- Processing of logs in the shared logging environment
+  - Access to S3 Buckets containing logs of security events
+  - Access to SQS Queues containing notifications of S3 Object Created Events
+
+Possible additions:
+- Processing of security events from AWS APIs
+  - GuardDuty
+  - SecurityHub
+- Asset collection
+  - EC2 Instance Metadata
---
+++ modules/security-operations-centre-role/main.tf
@@ -0,0 +1,43 @@
+resource "aws_iam_role" "this" {
+  name               = "${var.name_prefix}-security-operations-centre-role"
+  description        = "Delegate access to security operations centre to process security events."
+  assume_role_policy = data.aws_iam_policy_document.assume_role_policy.json
+  tags               = var.tags
+}
+
+// ExternalId is visible in the console, no point in making it secret
+resource "random_string" "externalid" {
+  length  = 16
+  special = true
+}
+
+data "aws_iam_policy_document" "assume_role_policy" {
+  statement {
+    effect = "Allow"
+
+    actions = [
+      "sts:AssumeRole",
+    ]
+
+    principals {
+      type = "AWS"
+
+      identifiers = [
+        var.security_operations_centre_aws_principal
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "sts:ExternalId"
+      values = [
+        random_string.externalid.result
+      ]
+    }
+  }
+}
+
+resource "aws_iam_role_policy_attachment" "test-attach" {
+  role       = aws_iam_role.this.name
+  policy_arn = aws_iam_policy.this.arn
+}
