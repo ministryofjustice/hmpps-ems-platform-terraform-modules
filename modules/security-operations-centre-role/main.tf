62c2ae37d50dd81eaabfc31bbecfef8b5d18ce4b
Ben Carter <ben.carter@justice.gov.uk>
2024-03-07 16:23:24 +0000

    ELM-1137: Update security operations role to allow additional access

--- modules/security-operations-centre-role/main.tf
+++ modules/security-operations-centre-role/main.tf
@@ -37,7 +37,7 @@ data "aws_iam_policy_document" "assume_role_policy" {
   }
 }
 
-resource "aws_iam_role_policy_attachment" "test-attach" {
+resource "aws_iam_role_policy_attachment" "this" {
   role       = aws_iam_role.this.name
   policy_arn = aws_iam_policy.this.arn
 }
