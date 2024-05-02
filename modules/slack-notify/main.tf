78ccaa00b2f79a42d7192e30a4dd7dd05b0ca552
suttonmashing <jack.sutton3@justice.gov.uk>
2024-05-02 13:07:15 +0000

    linting

--- modules/slack-notify/main.tf
+++ modules/slack-notify/main.tf
@@ -72,7 +72,7 @@ module "notify_slack" {
 
   lambda_role = "arn:aws:iam::${var.current_account}:role/lambda20200305133159295200000001"
 
-  slack_webhook_url = ${{ secrets.SLACK_WEBHOOK_URL }}
+  slack_webhook_url = secrets.SLACK_WEBHOOK_URL
   slack_channel     = "elmon-aws-alerts"
   slack_username    = var.current_account
   tags              = var.tags
