acdc620d2b781bbb69e045222b4af57a92230aca
SuttonMashing <98767768+SuttonMashing@users.noreply.github.com>
2024-05-22 14:57:18 +0100

    ELM-585: Create notify slack module (#75)
    * adds notify slack module to modules
    
    * moves slack webhook url into secrets
    
    * linting
    
    * linting
    
    * adds account number to module
    
    * linting
    
    * moves slack url and username to variables
    
    * lint
    
    * moves slack url and channel to variables
    
    * removes description from policy
    
    * lint
    
    * adds condition to sns topic policy
    
    * reverts last change
    
    * replaces variable with use of caller identity to get account number
    
    * moves account to statement in notify slack module
    
    * fix account referencing
    
    * fix account referencing
    
    * fix account referencing
    
    * fix account referencing
    
    * fix account referencing
    
    * linting
    
    * moves role name into a variable
    
    * remove hard coded role names
    
    * fix aws caller identity issue
    
    * linting
    
    * adds policy name to variables to allow it to be set
    
    * adds policy name to variables to allow it to be set
    
    * allows more flexibility in policy names for notify slack lambda
    
    * linting

---
+++ modules/slack-notify/variables.tf
@@ -0,0 +1,29 @@
+variable "tags" {
+  description = "A map of tags that will be applied to provisioned resources."
+  type        = map(string)
+}
+
+variable "slack_username" {
+  description = "Account name prefix"
+  type        = string
+}
+
+variable "slack_webhook_url" {
+  description = "Webhook URL for slack channel"
+  type        = string
+}
+
+variable "slack_channel" {
+  description = "Username for account on slack"
+  type        = string
+}
+
+variable "role_name" {
+  description = "The name for the lambda role"
+  type        = string
+}
+
+variable "policy_name" {
+  description = "The name for the lambda role policy"
+  type        = string
+}
