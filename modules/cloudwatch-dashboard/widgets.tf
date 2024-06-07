c486441e8e389bf1742d4fc340f2383830f0db55
Ben Carter <51202986+ben-cart3r@users.noreply.github.com>
2024-06-07 16:35:59 +0100

    BAU: Create a CloudWatch dashboard module (#76)

---
+++ modules/cloudwatch-dashboard/widgets.tf
@@ -0,0 +1,44 @@
+locals {
+  max_width    = 24 // CloudWatch dashboards use a 24 column grid
+  widget_width = local.max_width / var.column_count
+
+  metric_widgets = [
+    for index, alarm in var.metric_widgets : {
+      type   = "metric"
+      x      = (index % var.column_count) * local.widget_width
+      y      = (length(var.alarm_widgets) > 0 ? local.alarms_widget.height : 0) + floor(index / var.column_count) * var.widget_height
+      width  = local.widget_width
+      height = var.widget_height
+
+      properties = {
+        metrics   = alarm.metrics
+        period    = alarm.period
+        stat      = alarm.stat
+        region    = data.aws_region.current.name
+        title     = alarm.name
+        view      = alarm.view
+        stack     = alarm.stack
+        start     = alarm.start
+        end       = alarm.end
+        sparkline = alarm.sparkline
+      }
+    }
+  ]
+
+  alarms_widget = {
+    type   = "alarm",
+    x      = 0,
+    y      = 0,
+    width  = 24,
+    height = 4,
+    properties = {
+      title  = "Alarms",
+      alarms = var.alarm_widgets
+    }
+  }
+
+  widgets = concat(
+    length(var.alarm_widgets) > 0 ? [local.alarms_widget] : [],
+    local.metric_widgets
+  )
+}
