fbc28d5490b05c55f7fa484a769b464d1f13e083
Ben Carter <ben.carter@justice.gov.uk>
2024-08-07 16:00:09 +0000

    ELM-1804: Make creation optional

--- modules/pagerduty-integration/main.tf
+++ modules/pagerduty-integration/main.tf
@@ -9,7 +9,7 @@ resource "aws_sns_topic" "this" {
 resource "aws_sns_topic_subscription" "this" {
   count = var.create ? 1 : 0
 
-  topic_arn              = aws_sns_topic.this.arn
+  topic_arn              = aws_sns_topic.this[0].arn
   protocol               = "https"
   endpoint               = var.pagerduty_integration_url
   endpoint_auto_confirms = true
