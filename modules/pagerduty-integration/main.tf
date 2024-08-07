452c6dcfda3c5ba655e9be7cf454df6784da4a22
Ben Carter <ben.carter@justice.gov.uk>
2024-08-07 15:59:55 +0000

    ELM-1804: Make creation optional

--- modules/pagerduty-integration/main.tf
+++ modules/pagerduty-integration/main.tf
@@ -1,10 +1,14 @@
 resource "aws_sns_topic" "this" {
+  count = var.create ? 1 : 0
+
   name = "hmpps-sn-p-alarms-topic"
 
   tags = var.tags
 }
 
 resource "aws_sns_topic_subscription" "this" {
+  count = var.create ? 1 : 0
+
   topic_arn              = aws_sns_topic.this.arn
   protocol               = "https"
   endpoint               = var.pagerduty_integration_url
--- modules/pagerduty-integration/outputs.tf
+++ modules/pagerduty-integration/outputs.tf
@@ -1,4 +1,4 @@
 output "sns_topic_arn" {
   description = "The ARN of the SNS topic to send alerts to."
-  value       = aws_sns_topic.this.arn
+  value       = aws_sns_topic.this[0].arn
 }
