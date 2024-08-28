94ac2ca7ef75b0cf3bcf674ef02e9f6eda863846
Ben Carter <ben.carter@justice.gov.uk>
2024-08-28 14:02:50 +0000

    ELM-2425: Add aws_region data source

--- modules/account-baseline/modules/securityhub-standards/main.tf
+++ modules/account-baseline/modules/securityhub-standards/main.tf
@@ -1,3 +1,5 @@
+data "aws_region" "current" {}
+
 # Enable Standard: AWS Foundational Security Best Practices
 resource "aws_securityhub_standards_subscription" "aws-foundational" {
   standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
