970275a441e9b60488017770d61c96ab66d30432
Ben Carter <ben.carter@justice.gov.uk>
2024-08-28 13:25:53 +0000

    ELM-2425: Enable foundational and cis securityhub standards

---
+++ modules/account-baseline/modules/securityhub-standards/main.tf
@@ -0,0 +1,10 @@
+# Enable Standard: AWS Foundational Security Best Practices
+resource "aws_securityhub_standards_subscription" "aws-foundational" {
+  standards_arn = "arn:aws:securityhub:${data.aws_region.current.name}::standards/aws-foundational-security-best-practices/v/1.0.0"
+}
+
+# Enable Standard: CIS AWS Foundations
+resource "aws_securityhub_standards_subscription" "cis" {
+  standards_arn = "arn:aws:securityhub:::ruleset/cis-aws-foundations-benchmark/v/3.0.0"
+}
+
