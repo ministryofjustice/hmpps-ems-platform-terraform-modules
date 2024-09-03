d3ad20e8f4bc000f2732f0ccf801b9aae318ae7e
Ben Carter <ben.carter@justice.gov.uk>
2024-09-03 14:52:29 +0000

    ELM-2425: Enable creation of an aws support role

--- modules/account-baseline/aws-support.tf
+++ modules/account-baseline/aws-support.tf
@@ -2,6 +2,6 @@ module "aws_support" {
   count = var.create_aws_support_role ? 1 : 0
 
   source    = "./modules/aws-support"
-  role_name = "${resource_name_prefix}-support"
+  role_name = "${var.resource_name_prefix}-support"
   tags      = var.tags
 }
