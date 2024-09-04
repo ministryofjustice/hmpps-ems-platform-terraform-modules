9fb30e6acd7e758df18b10c2e4f1271810faf583
Ben Carter <ben.carter@justice.gov.uk>
2024-09-04 12:36:34 +0000

    ELM-2425: Allow config.amazonaws.com principal to write to config bucket

--- modules/account-baseline/modules/config/s3.tf
+++ modules/account-baseline/modules/config/s3.tf
@@ -71,7 +71,7 @@ data "aws_iam_policy_document" "bucket_policy" {
     ]
 
     resources = [
-      "arn:aws:s3:::${local.config_s3_bucket_name}"
+      "arn:aws:s3:::${local.config_s3_bucket_name}/AWSLogs/${data.aws_caller_identity.current.account_id}/Config/*"
     ]
 
     condition {
