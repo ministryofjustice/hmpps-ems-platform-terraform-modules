de84612a2eceb8ad6377d57a4bd679556ed725ae
Ben Carter <ben.carter@justice.gov.uk>
2024-09-04 12:13:07 +0000

    ELM-2425: Allow config.amazonaws.com principal to write to config bucket

--- modules/account-baseline/modules/config/s3.tf
+++ modules/account-baseline/modules/config/s3.tf
@@ -1,6 +1,101 @@
+data "aws_iam_policy_document" "bucket_policy" {
+  statement {
+    sid    = "AWSConfigBucketPermissionsCheck"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "config.amazonaws.com"
+      ]
+    }
+
+    actions = [
+      "s3:GetBucketAcl"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.config_s3_bucket_name}"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values = [
+        data.aws_caller_identity.current.account_id
+      ]
+    }
+  }
+
+  statement {
+    sid    = "AWSConfigBucketExistenceCheck"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "config.amazonaws.com"
+      ]
+    }
+
+    actions = [
+      "s3:ListBucket"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.config_s3_bucket_name}"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values = [
+        data.aws_caller_identity.current.account_id
+      ]
+    }
+  }
+
+  statement {
+    sid    = "AWSConfigBucketDelivery"
+    effect = "Allow"
+
+    principals {
+      type = "Service"
+      identifiers = [
+        "config.amazonaws.com"
+      ]
+    }
+
+    actions = [
+      "s3:PutObject"
+    ]
+
+    resources = [
+      "arn:aws:s3:::${local.config_s3_bucket_name}"
+    ]
+
+    condition {
+      test     = "StringEquals"
+      variable = "AWS:SourceAccount"
+      values = [
+        data.aws_caller_identity.current.account_id
+      ]
+    }
+
+    condition {
+      test     = "StringEquals"
+      variable = "s3:x-amz-acl"
+      values = [
+        "bucket-owner-full-control"
+      ]
+    }
+  }
+}
+
 module "config_s3_bucket" {
   source = "../../../standard-s3-bucket"
 
-  name = local.config_s3_bucket_name
-  tags = var.tags
+  name   = local.config_s3_bucket_name
+  policy = data.aws_iam_policy_document.bucket_policy.json
+  tags   = var.tags
 }
