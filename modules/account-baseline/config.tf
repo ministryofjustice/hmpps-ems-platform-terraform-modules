5369ffbdbfa0488b57c4d13939c0924daa300eed
Ben Carter <ben.carter@justice.gov.uk>
2024-09-04 14:11:00 +0000

    ELM-2425: Disable config in all regions besides eu-west-2

--- modules/account-baseline/config.tf
+++ modules/account-baseline/config.tf
@@ -13,20 +13,20 @@ module "config_bucket" {
 }
 
 # Enable config in all regions that SecurityHub is enabled
-module "config_eu_west_1" {
-  count  = var.enable_aws_config ? 1 : 0
+# module "config_eu_west_1" {
+#   count  = var.enable_aws_config ? 1 : 0
 
-  source = "./modules/config"
+#   source = "./modules/config"
   
-  providers = {
-    aws = aws.eu_west_1
-  }
+#   providers = {
+#     aws = aws.eu_west_1
+#   }
 
-  config_bucket_name   = module.config_bucket[0].bucket_id
-  config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
-  resource_name_prefix = var.resource_name_prefix
-  tags                 = var.tags
-}
+#   config_bucket_name   = module.config_bucket[0].bucket_id
+#   config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
+#   resource_name_prefix = var.resource_name_prefix
+#   tags                 = var.tags
+# }
 
 module "config_eu_west_2" {
   count  = var.enable_aws_config ? 1 : 0
@@ -43,47 +43,47 @@ module "config_eu_west_2" {
   tags                 = var.tags
 }
 
-module "config_eu_west_3" {
-  count  = var.enable_aws_config ? 1 : 0
+# module "config_eu_west_3" {
+#   count  = var.enable_aws_config ? 1 : 0
 
-  source = "./modules/config"
+#   source = "./modules/config"
   
-  providers = {
-    aws = aws.eu_west_3
-  }
+#   providers = {
+#     aws = aws.eu_west_3
+#   }
 
-  config_bucket_name   = module.config_bucket[0].bucket_id
-  config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
-  resource_name_prefix = var.resource_name_prefix
-  tags                 = var.tags
-}
+#   config_bucket_name   = module.config_bucket[0].bucket_id
+#   config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
+#   resource_name_prefix = var.resource_name_prefix
+#   tags                 = var.tags
+# }
 
-module "config_eu_central_1" {
-  count  = var.enable_aws_config ? 1 : 0
+# module "config_eu_central_1" {
+#   count  = var.enable_aws_config ? 1 : 0
 
-  source = "./modules/config"
+#   source = "./modules/config"
   
-  providers = {
-    aws = aws.eu_central_1
-  }
+#   providers = {
+#     aws = aws.eu_central_1
+#   }
 
-  config_bucket_name   = module.config_bucket[0].bucket_id
-  config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
-  resource_name_prefix = var.resource_name_prefix
-  tags                 = var.tags
-}
+#   config_bucket_name   = module.config_bucket[0].bucket_id
+#   config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
+#   resource_name_prefix = var.resource_name_prefix
+#   tags                 = var.tags
+# }
 
-module "config_us_east_1" {
-  count  = var.enable_aws_config ? 1 : 0
+# module "config_us_east_1" {
+#   count  = var.enable_aws_config ? 1 : 0
 
-  source = "./modules/config"
+#   source = "./modules/config"
   
-  providers = {
-    aws = aws.us_east_1
-  }
+#   providers = {
+#     aws = aws.us_east_1
+#   }
 
-  config_bucket_name   = module.config_bucket[0].bucket_id
-  config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
-  resource_name_prefix = var.resource_name_prefix
-  tags                 = var.tags
-}
+#   config_bucket_name   = module.config_bucket[0].bucket_id
+#   config_service_role  = aws_iam_service_linked_role.config_service_role[0].arn
+#   resource_name_prefix = var.resource_name_prefix
+#   tags                 = var.tags
+# }
